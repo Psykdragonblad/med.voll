81bf9fb3c24bad074ef510b391d545751d42f021
Marcos Souza <marcos.souza14@hotmail.com>
2024-09-14 22:20:11 -0300

    Criando o Repository e novos construtores

--- Api/src/main/java/med/voll/Api/controller/MedicoController.java
+++ Api/src/main/java/med/voll/Api/controller/MedicoController.java
@@ -1,6 +1,9 @@
 package med.voll.Api.controller;
 
 import med.voll.Api.medico.DadosCadastroMedicos;
+import med.voll.Api.medico.Medico;
+import med.voll.Api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedicos dados){
-        System.out.println(dados);
+
+        repository.save(new Medico(dados))
     }
 }
